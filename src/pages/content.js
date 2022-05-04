1b45e47bbe46681cc5d7a977f6e843f5598aa77a
Kyla Gail <kyzen.eth@Kyla.local>
2022-05-04 14:16:39 +0800

    4th 2nd

--- src/pages/content.js
+++ src/pages/content.js
@@ -5,7 +5,7 @@ export class Content extends Component {
     return (
       <div>
         <section class="hero body-font bg-purple-main text-gray-600">
-          <div class="container mx-auto flex flex-wrap px-5 py-24">
+          <div class="container mx-auto flex flex-wrap px-5 py-32">
             <div class="-mx-4 mt-auto mb-auto flex flex-wrap content-start sm:w-2/3 sm:pr-10 lg:w-1/2">
               <div class="mb-6 w-full px-4 sm:p-4 lg:text-left">
                 <h1 class="title-font mb-2 text-xl font-medium text-purple-box lg:text-4xl">humanDAO</h1>
@@ -18,12 +18,12 @@ export class Content extends Component {
             </div>
           </div>
         </section>
-        <section class="body-font bg-purple-content text-gray-600">
+        <section class="intro body-font bg-purple-content text-gray-600">
           <div class="flex w-full flex-col text-center">
-            <h1 class="mt-20 font-sans text-4xl font-bold text-white-lt lg:text-6xl">What does an HDAO PA do?</h1>
+            <h1 class="mx-8 mt-20 font-sans text-5xl font-bold text-white-lt lg:text-6xl">What does an HDAO PA do?</h1>
           </div>
-          <div class="container mx-auto px-5 py-40">
-            <div class="-m-4 flex flex-wrap">
+          <div class="container mx-auto px-5 py-24">
+            <div class="-m-2 flex flex-wrap">
               <div class="p-12 lg:w-1/3 lg:p-4">
                 <div class="relative h-full rounded-lg bg-purple-box bg-opacity-40 px-8 pt-16 pb-20 text-center">
                   <img class="relative mx-auto -mt-32 w-52" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625418/hdao-pa/3_mh4kih.png"></img>
@@ -34,15 +34,95 @@ export class Content extends Component {
               <div class="p-12 lg:w-1/3 lg:p-4">
                 <div class="relative h-full rounded-lg bg-purple-box bg-opacity-40 px-8 pt-16 pb-20 text-center">
                   <img class="relative mx-auto -mt-32 w-52" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625418/hdao-pa/4_or3wbg.png"></img>
-                  <h1 class="title-font mb-3 text-2xl font-bold text-pink-title sm:text-2xl">Works on day-to-day tasks</h1>
-                  <p class="mb-3 text-xl leading-tight text-white">A PA supports clients by doing day-to-day tasks in order to help them free up their time.</p>
+                  <h1 class="title-font mb-3 text-2xl font-bold text-pink-title sm:text-2xl">Research and collect data and info</h1>
+                  <p class="mb-3 text-xl leading-tight text-white">An HDAO PA works on tasks such as Data Entry and Research.</p>
                 </div>
               </div>
               <div class="p-12 lg:w-1/3 lg:p-4">
                 <div class="relative h-full rounded-lg bg-purple-box bg-opacity-40 px-8 pt-16 pb-20 text-center">
                   <img class="relative mx-auto -mt-32 w-52" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625418/hdao-pa/5_bfllic.png"></img>
-                  <h1 class="title-font mb-3 text-2xl font-bold text-pink-title sm:text-2xl">Works on day-to-day tasks</h1>
-                  <p class="mb-3 text-xl leading-tight text-white">A PA supports clients by doing day-to-day tasks in order to help them free up their time.</p>
+                  <h1 class="title-font mb-3 text-2xl font-bold text-pink-title sm:text-2xl">Work remotely and flexibly</h1>
+                  <p class="mb-3 text-xl leading-tight text-white">Being a PA is like doing gig work. You can select as many tasks as you want and work around your schedule.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="intro body-font bg-purple-content text-gray-600">
+          <div class="flex w-full flex-col text-center">
+            <h1 class="mx-8 mt-20 font-sans text-5xl font-bold text-white-lt lg:text-6xl">Type of Tasks</h1>
+          </div>
+          <div class="container mx-auto px-5 py-32">
+            <div class="-m-2 flex flex-wrap">
+              <div class="p-12 lg:w-1/2 lg:p-4">
+                <div class="relative h-full rounded-2xl bg-purple-box bg-opacity-40 px-8 pt-16 pb-20 text-center">
+                  <ul class="list-disc p-4 text-justify">
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="p-12 lg:w-1/2 lg:p-4">
+                <div class="relative h-full rounded-2xl bg-purple-box bg-opacity-40 px-8 pt-16 pb-20 text-center">
+                  <ul class="list-disc p-4 text-justify">
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                    <li class="text-white">Now this is a story all about how, my life got flipped turned upside down</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="intro body-font bg-purple-content text-gray-600">
+          <div class="flex w-full flex-col text-center">
+            <h1 class="mx-8 mt-20 font-sans text-5xl font-bold text-white-lt lg:text-6xl">Journey to becoming an HDAO PA</h1>
+          </div>
+          <div class="container mx-auto px-5 py-24">
+            <div class="-m-2 mx-8 flex flex-wrap rounded-2xl bg-purple-box/40 lg:mx-0">
+              <div class="p-12 lg:w-1/3 lg:p-4">
+                <div class="relative h-full rounded-lg bg-opacity-40 px-8 pt-16 pb-20 text-center">
+                  <img class="relative mx-auto w-60" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625418/hdao-pa/6_opuxa4.png"></img>
+                  <h1 class="title-font mb-3 text-4xl font-bold text-pink-title lg:text-4xl">Study and pass the HDAO PA exam</h1>
+                </div>
+              </div>
+              <div class="p-12 lg:w-1/3 lg:p-4">
+                <div class="relative h-full rounded-lg bg-opacity-40 px-8 pt-16 pb-20 text-center">
+                  <img class="relative mx-auto w-60" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625418/hdao-pa/7_wmvmxn.png"></img>
+                  <h1 class="title-font mb-3 text-4xl font-bold text-pink-title lg:text-4xl">Log in and start completing tasks</h1>
+                </div>
+              </div>
+              <div class="p-12 lg:w-1/3 lg:p-4">
+                <div class="relative h-full rounded-lg bg-opacity-40 px-8 pt-16 pb-20 text-center">
+                  <img class="relative mx-auto w-60" src="https://res.cloudinary.com/daljbo1q0/image/upload/v1651625419/hdao-pa/8_kb9cua.png"></img>
+                  <h1 class="title-font mb-3 text-4xl font-bold text-pink-title lg:text-4xl">Earn wages weekly from completed tasks</h1>
                 </div>
               </div>
             </div>
