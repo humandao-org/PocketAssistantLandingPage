6e7e48df88428db4949c7eb23f99cc419d4d3036
Kyla Gail <kyzen.eth@Kyla.local>
2022-05-04 10:50:14 +0800

    gh pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      "purple-bg": "#e8e2ff",
+      "purple-dark": "#2b1392",
+      "purple-text": "#6f5acd",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
