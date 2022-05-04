9fb4243e4803d5672f93c4d3cd1b5390b154297b
Kyla Gail <kyzen.eth@Kyla.local>
2022-05-04 22:42:39 +0800

    typo correction

--- src/pages/content.js
+++ src/pages/content.js
@@ -285,7 +285,7 @@ export class Content extends Component {
               <p class="md:text-normal mt-2 text-base text-white sm:text-lg">
                 There is an app you need to log into when you are available and ready to start completing tasks. Choose and accept the task(s) you will fulfill. When you accept a task it must be completed ASAP.
               </p>
-              <p class="md:text-normal mt-4 text-base text-white sm:text-lg">
+              <p class="md:text-normal mt-6 text-base text-white sm:text-lg">
                 When finished with task, you will upload any info or data to the client and mark the task as completed. A task should not take longer than 30 min. If it does, the client will have to use another task credit for completion.{" "}
               </p>
             </div>
@@ -302,7 +302,7 @@ export class Content extends Component {
                 That is entirely up to you. This is considered gig/freelance work. So the more tasks you complete the more you will make. Payouts will be weekly. We will also be rewarding PA's in both quality and quantity with a bonus every week.
                 Those that finish in the top 40% of tasks completed and feedback score will make both bonuses. Bonuses can easily 2x the earning for those who qualify.{" "}
               </p>
-              <p class="md:text-normal mt-4 text-base text-white sm:text-lg">
+              <p class="md:text-normal mt-6 text-base text-white sm:text-lg">
                 As we don’t know how successful this will be it is hard to know prior to launch how much each task will be worth, but we can provide a minimum/floor value which is $1 per task. Remember, a task could take 3 minutes or 30, and with
                 bonuses and rewards it could be 2-3x higher. We highly recommend you complete each of the course links provided. Each link should only take 1-1.5 hrs but knowing this info will help you complete the tasks quicker and with higher
                 quality, which means more wages.
